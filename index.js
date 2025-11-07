68d2068d911a1cbceb06548fd0bf0a6728e52030
Xtal-arch <isaacjp99@hotmail.co.uk>
2025-11-07 11:35:41 +0000

    Refactor convert function to handle input trimming

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const volumeConvert = document.getElementById("volume-convert")
 const massConvert = document.getElementById("mass-convert")
 
 function convert() {
-    const inputNum = document.getElementById("input-num").value
-    lengthConvert.innerHTML = `${inputNum.value} meters = ${inputNum.value * 3.281} feet | ${inputNum.value} feet = ${inputNum.value / 3.281} meters`
-    volumeConvert.innerHTML = `${inputNum.value} liters = ${inputNum.value * 0.264} gallons | ${inputNum.value} gallons = ${inputNum.value / 0.264} liters`
-    massConvert.innerHTML = `${inputNum.value} kilos = ${inputNum.value * 2.204} pounds | ${inputNum.value} pounds = ${inputNum.value / 2.204} kilos`
+    const raw = document.getElementById("input-num").value.trim();
+    const num  = Number(raw);
+    lengthConvert.innerHTML = `${num} meters = ${num * 3.281} feet | ${num} feet = ${num / 3.281} meters`;
+    volumeConvert.innerHTML = `${num} liters = ${num * 0.264} gallons | ${num} gallons = ${num / 0.264} liters`;
+    massConvert.innerHTML = `${num} kilos = ${num * 2.204} pounds | ${num} pounds = ${num / 2.204} kilos`;
 }
 
 convertBtn.addEventListener("click", convert) 
